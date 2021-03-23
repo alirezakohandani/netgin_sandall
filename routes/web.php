3e0ee86a39f6935e54e18f544220516849354a74
alirezakohandani <alireza.ko73@gmail.com>
2021-03-23 17:50:55 +0430

    Create register controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [MainController::class, 'index']);
+
+Route::group(['prefix' => 'auth', 'as' => 'auth'], function() {
+    Route::get('register', [RegisterController::class, 'registerationForm'])->name('register.form');
+});
